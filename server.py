5f4a83920d467bb8d054e63419536aa8f5961dfb
Alex Soares <alealvessoares28@gmai.com>
2024-01-31 22:44:43 -0300

    adicionado metadados e comentarios

--- server.py
+++ server.py
@@ -1,25 +1,41 @@
 #!/usr/bin/env python3
+"""
+server.py - Script para criar um servidor de sockets TCP.
+"""
+__version__ = "0.1.0"
+__author__ = "Alex Soares"
+__license__ = "Unlicense"
 
 import socket #importa a lib de sockets
 
 def server():
 
-    data_payload = 1024
+    # Tamanho máximo dos dados que podem ser recebidos de uma vez
+    data_payload = 1024 
 
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #criando o socket, AF_INET se refere a familia do socket e o SOCK_STREAM ao tipo, nesse caso tipo tcp
+    # Criação do socket TCP
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    server_socket.bind(("localhost", 9473)) #com o metodo bind informamos o IP e porta onde o socket é criado
+    # Associação do socket ao endereço local e à porta
+    server_socket.bind(("localhost", 9473))
 
-    server_socket.listen() #Faz com que o o socket aguarde uma conexão
+    # Aguarda conexões
+    server_socket.listen()
 
-    connection, address = server_socket.accept() #Mostra quem esta se conectando
+    # Aceita a conexão quando um cliente se conecta
+    connection, address = server_socket.accept()
 
     while True:
-        data = connection.recv(data_payload) #O metodo recv recebe os dados e tem como parametro o tamanho das mensagem
+        # Recebe os dados do cliente
+        data = connection.recv(data_payload) 
         if not data:
             break
-        print(f"Nova mensagem do host {address}: {data.decode()}") #Print o host que esta se conectando e a mensagem
-        
+        # Exibe a mensagem recebida e o endereço do cliente
+        print(f"Nova mensagem do host {address}: {data.decode()}")
+
+    #Encerra a conexão
     connection.close() #Encerra a conexão
  
-server() #Chama a função
+
+if __name__ == "__main__":
+    server()
